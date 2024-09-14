df9369036db05ec05239bc310214500c1ace3c39
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-09-14 17:50:25 +0200

    Update search_ameco.R

--- R/search_ameco.R
+++ R/search_ameco.R
@@ -1,5 +1,5 @@
 search_ameco <- function(expresion) {
     data <- ameco::ameco_series_df %>%
-        filter(str_detect(str_to_lower(title), str_to_lower(expresion)))
+        filter(str_detect(str_to_lower(title), str_to_lower(expresion)) | str_detect(str_to_lower(series), str_to_lower(expresion)))
     return(data)
 }
